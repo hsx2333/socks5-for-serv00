4570f2daf8a0a2003654594579ae7c53fef07549
回首向 <40594978+hsx2333@users.noreply.github.com>
2024-09-17 15:27:46 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 USER=$(whoami)
-WORKDIR="/home/${USER}/.nezha-agent"
-FILE_PATH="/home/${USER}/1"
-CRON_V2bX="nohup ${FILE_PATH}/V2bX server -c config.json >/dev/null 2>&1 &"
+WORKDIR="/home/${USER,,}/.nezha-agent"
+FILE_PATH="/home/${USER,,}/1"
+CRON_V2bX="nohup ${FILE_PATH}/V2bX server -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
 CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
-PM2_PATH="/home/${USER}/.npm-global/lib/node_modules/pm2/bin/pm2"
+PM2_PATH="/home/${USER,,}/.npm-global/lib/node_modules/pm2/bin/pm2"
 CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 
 echo "检查并添加 crontab 任务"
 
-if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
+if [ "$(command -v pm2)" == "/home/${USER,,}/.npm-global/bin/pm2" ]; then
   echo "已安装 pm2，并返回正确路径，启用 pm2 保活任务"
   (crontab -l | grep -F "$REBOOT_COMMAND") || (crontab -l; echo "$REBOOT_COMMAND") | crontab -
   (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
 else
   if [ -e "${WORKDIR}/start.sh" ] && [ -e "${FILE_PATH}/config.json" ]; then
-    echo "添加 nezha & sockV2bX 的 crontab 重启任务"
+    echo "添加 nezha & V2bX 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_V2bX} && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_V2bX} && ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"V2bX\" > /dev/null || ${CRON_V2bX}") || (crontab -l; echo "*/12 * * * * pgrep -x \"V2bX\" > /dev/null || ${CRON_V2bX}") | crontab -
@@ -26,8 +26,10 @@ else
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
   elif [ -e "${FILE_PATH}/config.json" ]; then
-    echo "添加 sockV2bX 的 crontab 重启任务"
+    echo "添加 V2bX 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_V2bX}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_V2bX}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"V2bX\" > /dev/null || ${CRON_V2bX}") || (crontab -l; echo "*/12 * * * * pgrep -x \"V2bX\" > /dev/null || ${CRON_V2bX}") | crontab -
   fi
 fi
+
+echo "致谢：hsx"
